c2f409ba29d87d2d6983e284c13142701362e6b4
m-altieri <m.altieri13@studenti.uniba.it>
2017-11-13 19:48:37 +0100

    parametri

--- recsysbot/config/movierecsysbot-config.php
+++ recsysbot/config/movierecsysbot-config.php
@@ -4,7 +4,7 @@
  */
 return [
     // Telegram TOKEN movierecsysbot
-    'token' => '382351414:AAFBuLhmBcma63wcVXQxbgUca_HqRKCff_k',
+    'token' => '422658992:AAH1c7kkVvuAIIuVDbbek7Mo4Zd0pKSU8nM',
     //The timezone setting, Guzzle suggests having this for proper requests/responses
     'timezone' => 'Europe/Rome',
     //If no response is found, this will be used as response
